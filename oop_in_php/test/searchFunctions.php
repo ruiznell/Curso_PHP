cc07f42d92c8b36ab9f4e7b013f328d1979e05e1
ruiznell <ruiznell@gmail.com>
2021-03-24 15:16:48 +0100

    impostazione branch mio-test-isexpired

---
+++ oop_in_php/test/searchFunctions.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Funzione di ordine superiore che restituisce una funzione
+ * Programmazione Funzionale - dichiarativo
+ */
+
+function searchText($searchText){
+    
+    return function ($taskItem) use ($searchText){
+
+    $noSpaces = preg_replace('/[ ]+/m', ' ', $searchText);
+    $searchSpaces = trim($noSpaces);
+    
+    if ($searchSpaces !== '')
+    {    
+        $result = stripos($taskItem['taskName'], $searchSpaces) !== false;
+        
+    } else {
+        $result = true;
+    }
+        return $result;    
+           
+    };
+
+}
+
+
+/**
+ * @var string $status è la stringa che corrisponde allo status da cercare
+ * (progress|done|todo)
+ * @return callable La funzione che verrà utilizzata da array_filter
+ */
+// function searchStatus(string $status) : callable {
+    
+// } 
+
+
+function searchStatus($status){
+
+    return function ($taskItem) use ($status){ 
+        if ($status===""){
+            $result = count($taskItem);
+        }else{
+            if ($status!=='all') {
+                $result = strpos($taskItem['status'], $status) !==false;
+            }else{
+                $result = count($taskItem);
+            }
+        }
+        return $result;
+    };
+}
+
+function coloreStatus($status)
+    {
+        
+     if ($status === "todo")
+        {
+            return "danger";
+        }
+    
+        elseif ($status === "progress")
+        {
+            return "primary";
+        } 
+    
+     else
+        {
+            return "secondary";
+        }
+    }
+
+    function isExpired():bool
+
+{
+              //$today è una istanza della classe DateTime
+    $today = new DateTime();
+    $task = new DateTime($this->expirationDate);
+    //con esta operacion se puede manipular la data
+    
+    return $task < $today;
+}
+?>
